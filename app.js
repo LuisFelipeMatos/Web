3190153070c906808842c83e335b8f0d264f9cd7
LuisFelipeMatos <60949831+LuisFelipeMatos@users.noreply.github.com>
2021-03-15 19:05:59 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const usuario = require('./rotas/usuario');
-const nota = require('./rotas/nota');
+const usuario = require('./routes/usuario');
+const nota = require('./routes/nota');
 const checks = require('./routes/checklist');
 const tags = require('./routes/tag')
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+
 
 app.use('/checklist', checks);
 app.use('/tag', tags);
 app.use('/usuario', usuario);
 app.use('/nota', nota);
+app.use(bodyParser.json());
 
 
 
